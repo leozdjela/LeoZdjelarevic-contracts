e0f14ba94f38ed1beb93561e7996efeee7f98837
Leo Zdjelarevic <116094883+leozdjela@users.noreply.github.com>
2025-11-27 16:45:52 +0100

    Deploying contracts
    Deploying my contract in cmd.
    
    All the properties bla bla ba

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,19 +3,32 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach (() => {
-    //Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+const compiled = require('../compile');
+const inboxContract = compiled[':Inbox'];
 
-    //Use one of those accounts
-    //to deploy the contract
+const abi = JSON.parse(inboxContract.interface);
+const bytecode = inboxContract.bytecode;
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {
-        
-    });
+  it('deploys a contract', () => {
+    console.log("============= CONTRACT INFO =============");
+    console.log("ABI:\n", JSON.stringify(abi, null, 2));
+    console.log("\nBYTECODE:\n", bytecode);
+    console.log("\nDEPLOYED ADDRESS:\n", inbox.options.address);
+    console.log("=========================================");
+    
+    // Optional assertion:
+    assert.ok(inbox.options.address);
+  });
 });
