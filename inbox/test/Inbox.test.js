9d32bf6a2657bc16c3cd315302cf371993780826
Leo Zdjelarevic <116094883+leozdjela@users.noreply.github.com>
2025-11-27 15:26:43 +0100

    Testing
    Testing mocha, added ganache and web3 modules.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,31 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
 
-// updated ganache and web3 imports added for convenience
+//TESTING FOR MOCHA
+// class Car{
+//     park(){
+//         return 'stopped';
+//     }
 
-// contract test code will go here
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//      car = new Car();
+// });
+
+// describe ('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     } );
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//         });
+// });
