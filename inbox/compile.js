9069f36833d56d7dd258fadd015a39d9511cc0ca
Leo Zdjelarevic <116094883+leozdjela@users.noreply.github.com>
2025-11-28 15:48:32 +0100

    Changes
    changed compile.js
    because it couldnt deploy contract
    had to put module.exports for interface and bytecode

--- inbox/compile.js
+++ inbox/compile.js
@@ -8,7 +8,12 @@ const source = fs.readFileSync(inboxPath, 'utf8');
 // Use legacy compile
 const compiled = solc.compile(source, 1);
 
+// Debug logs to confirm
 console.log("Compiled keys:", Object.keys(compiled.contracts));
-console.log("Full contracts object:", compiled.contracts);
 
-module.exports = compiled.contracts;
+const contract = compiled.contracts[':Inbox'];
+
+module.exports = {
+    interface: contract.interface,
+    bytecode: contract.bytecode
+};
--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,25 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+    'ask insane dust radar gallery ordinary globe festival fuel tag flag diet',
+    'https://sepolia.infura.io/v3/6d75e5fcb6454f1199d0a67721d8ef68'
+    
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({gas: '1000000', from: accounts[0]});
+
+        console.log('Contract deployed to', result.options.address);
+        provider.engine.stop();
+};
+deploy();
